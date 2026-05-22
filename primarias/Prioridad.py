6f492475031d019613c332f8b813bd5e74dc7182
Michelle Salgado <msalgado77@unisalle.edu.co>
2026-05-21 21:41:31 -0500

    Agregando metodos y mejorando el formato

--- primarias/Prioridad.py
+++ primarias/Prioridad.py
@@ -1,101 +1,53 @@
+from vias import Vias
+
+
 class Prioridad:
 
-    def _init_(self, estado_actual: str, criterios_daños: str, promedio_circulacion: int, promedio_accidentes: int)-> None:
+    def _init_(
+        self,
+        estado_actual: str,
+        criterios_daños: str,
+        promedio_circulacion: int,
+        promedio_accidentes: int,
+        via: Vias,
+    ) -> None:
 
+        self.via = via
         self.estado = estado_actual
         self.daños = criterios_daños
         self.circulacion = promedio_circulacion
         self.accidentes = promedio_accidentes
+        self.puntaje_prioridad = 0
 
     def mostrar_informacion(self) -> str:
-       
         return (
-            f"Estado de la vía: {self.estado}, "
-            f"Promedio de accidentes: {self.accidentes}, "
-            f"Promedio de circulación: {self.circulacion},"
+            f"Vía: {self.via.nombre}\n"
+            f"Estado de la vía: {self.estado}\n"
+            f"Promedio de accidentes: {self.accidentes}\n"
+            f"Promedio de circulación: {self.circulacion}\n"
             f"Criterios de daños causados a la vía: {self.daños}"
         )
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def actualizar_informacion(
+        self,
+        estado_actual: str,
+        criterios_daños: str,
+        promedio_circulacion: int,
+        promedio_accidentes: int,
+    ) -> None:
+        self.estado = estado_actual
+        self.daños = criterios_daños
+        self.circulacion = promedio_circulacion
+        self.accidentes = promedio_accidentes
+        self.asignar_puntaje()
+
+    def asignar_puntaje(self) -> None:
+        self.puntaje_prioridad = self.accidentes + self.circulacion
+
+    def comparar_prioridad(self, otra_prioridad: "Prioridad") -> str:
+        if self.puntaje_prioridad > otra_prioridad.puntaje_prioridad:
+            return f"La via {self.via} tiene mayor prioridad."
+        elif self.puntaje_prioridad < otra_prioridad.puntaje_prioridad:
+            return f"La via {otra_prioridad.via} tiene mayor prioridad."
+        else:
+            return "Ambas vías tienen la misma prioridad."
