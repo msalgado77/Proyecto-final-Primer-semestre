9e4fea25a1d6e4b3fc1f033c10b3661966146c62
Michelle Salgado <msalgado77@unisalle.edu.co>
2026-05-26 18:24:28 -0500

    Hacer un historial y mostrarlo

--- Prueba_final.py
+++ Prueba_final.py
@@ -3,6 +3,7 @@ from primarias.vias import Vias
 from primarias.Prioridad import Prioridad
 from primarias.Mantenimientos import Mantenimientos
 from secundarias.Presupuesto import Presupuesto
+from secundarias.Historial_mantenimiento import Hismantenimiento
 
 print("========================================")
 print("=== INTEGRACIÓN TOTAL DEL PROYECTO ===")
@@ -11,9 +12,8 @@ print("========================================\n")
 # Creamos una Vía
 
 via_principal = Vias(
-    "Avenida Central", "Norte", 100.5, 10.5, "Principal", "Asfalto", 
-    "15/05/2026"
-    )
+    "Avenida Central", "Norte", 100.5, 10.5, "Principal", "Asfalto", "15/05/2026"
+)
 
 prioridad_avenida = Prioridad("Crítico", "Huecos profundos", 15, 10, via_principal)
 prioridad_avenida.asignar_puntaje()
@@ -32,3 +32,20 @@ print("\n")
 presupuesto_alcaldia = Presupuesto(15000)
 presupuesto_alcaldia.emparejar_con_mantenimientos(mantenimiento_avenida)
 print("\n")
+
+
+historial_avenida = Hismantenimiento("Avenida Central", 15, "Recarpeteo", 7000)
+
+historial_avenida.agregar_registro("Reparación de huecos profundos")
+historial_avenida.agregar_registro("Reemplazo de señalización")
+historial_avenida.agregar_registro("Recarpeteo con asfalto")
+
+
+print("------Historial de Mantenimientos------")
+print(historial_avenida.mostrar_historial())
+
+
+print("\n------Datos del mantenimiento------")
+print(f"Tiempo desde último mantenimiento: {historial_avenida.tiempo} días")
+print(f"Tipo de mantenimiento: {historial_avenida.tipo}")
+print(f"Costo total: ${historial_avenida.costo}")
--- secundarias/Historial_mantenimiento.py
+++ secundarias/Historial_mantenimiento.py
@@ -1,12 +1,13 @@
 class Hismantenimiento:
 
-    def _init_(
+    def __init__(
         self,
+        via: str,
         tiempo_ulmantenimiento: int,
         tipo_mantenimiento: str,
         costo_mantenimiento: int,
-    ) -> None:
-
+    ):
+        self.via = via
         self.tiempo = tiempo_ulmantenimiento
         self.tipo = tipo_mantenimiento
         self.costo = costo_mantenimiento
@@ -14,6 +15,14 @@ class Hismantenimiento:
 
     def agregar_registro(self, descripcion):
         self.registros.append(descripcion)
+
+    def mostrar_historial(self):
+        historial = "Historial de mantenimientos:\n"
+        for i, registro in enumerate(self.registros, start=1):
+            historial += f"{i}. {registro}\n"
+        return historial
+
+
 
     def mostrar_informacion(self) -> str:
 
