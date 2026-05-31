7b22d6aa3cc0d3ad30d60f5c427b819ad8e82ca9
Michelle Salgado <msalgado77@unisalle.edu.co>
2026-05-31 12:35:06 -0500

    Arreglo a un codigo rrpetido

--- secundarias/Historial_mantenimiento.py
+++ secundarias/Historial_mantenimiento.py
@@ -42,11 +42,3 @@ class Hismantenimiento:
         self.tiempo = tiempo_ulmantenimiento
         self.tipo = tipo_mantenimiento
         self.costo = costo_mantenimiento
-
-    def mostrar_historial(self):
-        if not self.registros:
-            return f"No hay mantenimientos registrados para la vía {self.via}."
-        historial = f"Historial de mantenimientos para {self.via}:\n"
-        for i, registro in enumerate(self.registros, 1):
-            historial += f"{i}. {registro}\n"
-        return historial
